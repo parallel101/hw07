d47360ea7cc492f23dff24cac8f5079b01bc23e2
archibate <1931127624@qq.com>
2022-01-23 00:16:12 +0800

    add hw07

--- main.cpp
+++ main.cpp
@@ -1,6 +1,116 @@
-#include <cstdio>
+#include <iostream>
+//#include <x86intrin.h>  // _mm 系列指令都来自这个头文件
+//#include <xmmintrin.h>  // 如果上面那个不行，试试这个
+#include "ndarray.h"
+#include "wangsrng.h"
+#include "ticktock.h"
+
+// Matrix 是 YX 序的二维浮点数组：mat(x, y) = mat.data()[y * mat.shape(0) + x]
+using Matrix = ndarray<2, float>;
+// 注意：默认对齐到 64 字节，如需 4096 字节，请用 ndarray<2, float, AlignedAllocator<4096, float>>
+// 并行可以用 OpenMP 也可以用 TBB
+
+static void matrix_randomize(Matrix &out) {
+    TICK(matrix_randomize);
+    size_t nx = out.shape(0);
+    size_t ny = out.shape(1);
+
+    // 这个循环为什么不够高效？如何优化？
+#pragma omp parallel for collapse(2)
+    for (int x = 0; x < nx; x++) {
+        for (int y = 0; y < ny; y++) {
+            float val = wangsrng(x, y).next_float();
+            out(x, y) = val;
+        }
+    }
+    TOCK(matrix_randomize);
+}
+
+static void matrix_transpose(Matrix &out, Matrix const &in) {
+    TICK(matrix_transpose);
+    size_t nx = in.shape(0);
+    size_t ny = in.shape(1);
+    out.reshape(ny, nx);
+
+    // 这个循环为什么不够高效？如何优化？
+#pragma omp parallel for collapse(2)
+    for (int x = 0; x < nx; x++) {
+        for (int y = 0; y < ny; y++) {
+            out(y, x) = in(x, y);
+        }
+    }
+    TOCK(matrix_transpose);
+}
+
+static void matrix_multiply(Matrix &out, Matrix const &lhs, Matrix const &rhs) {
+    TICK(matrix_multiply);
+    size_t nx = lhs.shape(0);
+    size_t nt = lhs.shape(1);
+    size_t ny = rhs.shape(1);
+    if (rhs.shape(0) != nt) {
+        std::cerr << "matrix_multiply: shape mismatch" << std::endl;
+        throw;
+    }
+    out.reshape(nx, ny);
+
+    // 这个循环为什么不够高效？如何优化？
+#pragma omp parallel for collapse(2)
+    for (int y = 0; y < ny; y++) {
+        for (int x = 0; x < nx; x++) {
+            out(x, y) = 0;  // 有没有必要手动初始化？提示：ndarray 的底层是 std::vector
+            for (int t = 0; t < nt; t++) {
+                out(x, y) += lhs(x, t) * rhs(t, y);
+            }
+        }
+    }
+    TOCK(matrix_multiply);
+}
+
+// 求出 R^T A R
+static void matrix_RtAR(Matrix &RtAR, Matrix const &R, Matrix const &A) {
+    TICK(matrix_RtAR);
+    Matrix Rt;       // 这是临时变量，有什么可以优化的？
+    matrix_transpose(Rt, R);
+    Matrix RtA;      // 这是临时变量，有什么可以优化的？
+    matrix_multiply(RtA, Rt, A);
+    matrix_multiply(RtAR, RtA, R);
+    TOCK(matrix_RtAR);
+}
+
+static float matrix_trace(Matrix const &in) {
+    TICK(matrix_trace);
+    float res = 0;
+    size_t nt = std::min(in.shape(0), in.shape(1));
+#pragma omp parallel for reduction(+:res)
+    for (int t = 0; t < nt; t++) {
+        res += in(t, t);
+    }
+    TOCK(matrix_trace);
+    return res;
+}
+
+static void test_func(size_t n) {
+    TICK(test_func);
+    Matrix R(n, n);
+    matrix_randomize(R);
+    Matrix A(n, n);
+    matrix_randomize(A);
+
+    Matrix RtAR;
+    matrix_RtAR(RtAR, R, A);
+
+    std::cout << matrix_trace(RtAR) << std::endl;
+    TOCK(test_func);
+}
 
 int main() {
-    printf("Hello, world!\n");
+    wangsrng rng;
+    TICK(overall);
+    for (int t = 0; t < 4; t++) {
+        size_t n = 32 * (rng.next_uint64() % 16 + 24);
+        std::cout << "t=" << t << ": n=" << n << std::endl;
+        test_func(n);
+    }
+    TOCK(overall);
     return 0;
 }
