f3861b4ecfdbc16b2aa55265d1db2cc0fc96f0e1
taiyang-li <654010905@qq.com>
2022-07-10 18:04:56 +0800

    finish

--- ANSWER.md
+++ ANSWER.md
@@ -1,25 +1,101 @@
 # 改进前
 
 ```
-这里贴改进前的运行结果。
-matrix_randomize: 100s
+t=0: n=1120
+matrix_randomize: 0.0791472s
+matrix_randomize: 0.0525622s
+matrix_transpose: 0.0595276s
+matrix_multiply: 0.974533s
+matrix_multiply: 0.7107s
+matrix_RtAR: 1.74504s
+matrix_trace: 0.0297299s
+1.75932e+08
+test_func: 1.91395s
+t=1: n=928
+matrix_randomize: 0.0284199s
+matrix_randomize: 0.0295304s
+matrix_transpose: 0.0318627s
+matrix_multiply: 0.18651s
+matrix_multiply: 0.177179s
+matrix_RtAR: 0.395645s
+matrix_trace: 0.0200901s
+1.00156e+08
+test_func: 0.478293s
+t=2: n=1024
+matrix_randomize: 0.0242934s
+matrix_randomize: 0.0295988s
+matrix_transpose: 0.0409102s
+matrix_multiply: 0.469459s
+matrix_multiply: 0.526811s
+matrix_RtAR: 1.03738s
+matrix_trace: 0.0315037s
+1.34324e+08
+test_func: 1.12798s
+t=3: n=1056
+matrix_randomize: 0.0323473s
+matrix_randomize: 0.0290094s
+matrix_transpose: 0.0358525s
+matrix_multiply: 0.528684s
+matrix_multiply: 0.538887s
+matrix_RtAR: 1.10351s
+matrix_trace: 0.03619s
+1.47405e+08
+test_func: 1.20674s
+overall: 4.73322s
 ```
 
 # 改进后
 
 ```
-这里贴改进后的运行结果。
-matrix_randomize: 0.01s
+t=0: n=1120
+matrix_randomize: 0.000528096s
+matrix_randomize: 0.000535718s
+matrix_transpose: 0.0022195s
+matrix_multiply: 0.160545s
+matrix_multiply: 0.127222s
+matrix_RtAR: 0.296209s
+matrix_trace: 0.0278956s
+1.75932e+08
+test_func: 0.331247s
+t=1: n=928
+matrix_randomize: 0.000358425s
+matrix_randomize: 0.000367327s
+matrix_transpose: 0.0012165s
+matrix_multiply: 0.0528992s
+matrix_multiply: 0.0683825s
+matrix_RtAR: 0.122551s
+matrix_trace: 0.043051s
+1.00156e+08
+test_func: 0.170517s
+t=2: n=1024
+matrix_randomize: 0.000449409s
+matrix_randomize: 0.000480594s
+matrix_transpose: 0.0016001s
+matrix_multiply: 0.0647215s
+matrix_multiply: 0.091688s
+matrix_RtAR: 0.158071s
+matrix_trace: 0.0399191s
+1.34324e+08
+test_func: 0.203998s
+t=3: n=1056
+matrix_randomize: 0.000623919s
+matrix_randomize: 0.000620208s
+matrix_transpose: 0.00164745s
+matrix_multiply: 0.0739627s
+matrix_multiply: 0.0657667s
+matrix_RtAR: 0.141427s
+matrix_trace: 0.0281347s
+1.47405e+08
+test_func: 0.176396s
+overall: 0.883619s
 ```
 
 # 加速比
 
-matrix_randomize: 10000x
-matrix_transpose: 10000x
-matrix_multiply: 10000x
-matrix_RtAR: 10000x
-
-> 如果记录了多种优化方法，可以做表格比较
+matrix_randomize: 46x到150x
+matrix_transpose: 21x到27x
+matrix_multiply:  2.5x到8.2x
+matrix_RtAR:      3.2x到7.8x
 
 # 优化方法
 
@@ -27,20 +103,31 @@ matrix_RtAR: 10000x
 
 > matrix_randomize
 
-请回答。
+尝试过的优化手段
+- 先遍历y,后遍历x: 增强空间局部性，提高cpu cache命令率
+- _mm_stream_si32或_mm512_stream_ps (无提升)
+- matrix对齐到4096 (无提升)
+
 
 > matrix_transpose
 
-请回答。
+尝试过的优化手段
+- 分块计算
+ 
 
 > matrix_multiply
 
-请回答。
+尝试过的优化手段
+- 寄存器分块，步长32 
+- openmp多线程
+- gcc循环展开
+- 去掉对out(x,y)赋值为零的操作。(提升不明显)
 
 > matrix_RtAR
 
-请回答。
+尝试过的优化手段
+- 去掉临时变量，改成static, 加上初始化
+
 
 # 我的创新点
-
-如果有，请说明。
+暂无
--- main.cpp
+++ main.cpp
@@ -8,28 +8,62 @@
 // 并行可以用 OpenMP 也可以用 TBB
 
 #include <iostream>
-//#include <x86intrin.h>  // _mm 系列指令都来自这个头文件
+#include <x86intrin.h> // _mm 系列指令都来自这个头文件
 //#include <xmmintrin.h>  // 如果上面那个不行，试试这个
+#include <tbb/blocked_range2d.h>
+#include <tbb/parallel_for.h>
+#include "alignalloc.h"
 #include "ndarray.h"
-#include "wangsrng.h"
 #include "ticktock.h"
+#include "wangsrng.h"
 
 // Matrix 是 YX 序的二维浮点数组：mat(x, y) = mat.data()[y * mat.shape(0) + x]
 using Matrix = ndarray<2, float>;
-// 注意：默认对齐到 64 字节，如需 4096 字节，请用 ndarray<2, float, AlignedAllocator<4096, float>>
+// using Matrix = ndarray<2, float, 0, 0, AlignedAllocator<float, 4096>>;
+// 注意：默认对齐到 64 字节，如需 4096 字节，请用 ndarray<2, float,
+// AlignedAllocator<4096, float>>
 
 static void matrix_randomize(Matrix &out) {
     TICK(matrix_randomize);
     size_t nx = out.shape(0);
     size_t ny = out.shape(1);
 
-    // 这个循环为什么不够高效？如何优化？ 10 分
-#pragma omp parallel for collapse(2)
-    for (int x = 0; x < nx; x++) {
-        for (int y = 0; y < ny; y++) {
-            float val = wangsrng(x, y).next_float();
-            out(x, y) = val;
+    size_t base = 0;
+    /*
+    for (; base < nx * ny; base += 16) {
+        auto res = _mm512_set_ps(
+            wangsrng((base + 15) % nx, (base + 15) / nx).next_float(),
+            wangsrng((base + 14) % nx, (base + 14) / nx).next_float(),
+            wangsrng((base + 13) % nx, (base + 13) / nx).next_float(),
+            wangsrng((base + 12) % nx, (base + 12) / nx).next_float(),
+            wangsrng((base + 11) % nx, (base + 11) / nx).next_float(),
+            wangsrng((base + 10) % nx, (base + 10) / nx).next_float(),
+            wangsrng((base + 9) % nx, (base + 9) / nx).next_float(),
+            wangsrng((base + 8) % nx, (base + 8) / nx).next_float(),
+            wangsrng((base + 7) % nx, (base + 7) / nx).next_float(),
+            wangsrng((base + 6) % nx, (base + 6) / nx).next_float(),
+            wangsrng((base + 5) % nx, (base + 5) / nx).next_float(),
+            wangsrng((base + 4) % nx, (base + 4) / nx).next_float(),
+            wangsrng((base + 3) % nx, (base + 3) / nx).next_float(),
+            wangsrng((base + 2) % nx, (base + 2) / nx).next_float(),
+            wangsrng((base + 1) % nx, (base + 1) / nx).next_float(),
+            wangsrng((base + 0) % nx, (base + 0) / nx).next_float());
+        _mm512_stream_ps(&out.data()[base], res);
+    }
+    for (int y = 0; y < ny; y++) {
+        for (int x = 0; x < nx; x++) {
+            float value = wangsrng(x, y).next_float();
+            _mm_stream_si32((int *)&out(x, y), *(int *)&value);
         }
+    }
+    */
+    // 这个循环为什么不够高效？如何优化？ 10 分
+// #pragma omp parallel for collapse(2) num_threads(2)
+    for (int y = 0; y < ny; y++) {
+      for (int x = 0; x < nx; x++) {
+        float val = wangsrng(x, y).next_float();
+        out(x, y) = val;
+      }
     }
     TOCK(matrix_randomize);
 }
@@ -40,13 +74,17 @@ static void matrix_transpose(Matrix &out, Matrix const &in) {
     size_t ny = in.shape(1);
     out.reshape(ny, nx);
 
-    // 这个循环为什么不够高效？如何优化？ 15 分
-#pragma omp parallel for collapse(2)
-    for (int x = 0; x < nx; x++) {
-        for (int y = 0; y < ny; y++) {
-            out(y, x) = in(x, y);
-        }
-    }
+    tbb::parallel_for(
+        tbb::blocked_range2d<size_t>(0, nx, 64, 0, ny, 64),
+        [&](tbb::blocked_range2d<size_t> const &r) {
+            for (int x = r.rows().begin(); x < r.rows().end(); ++x) {
+                for (int y = r.cols().begin(); y < r.cols().end(); ++y) {
+                    // _mm_stream_si32((int *)&out(y, x), *(int *)&in(x,y));
+                    out(y, x) = in(x, y);
+                }
+            }
+        },
+        tbb::simple_partitioner{});
     TOCK(matrix_transpose);
 }
 
@@ -61,16 +99,32 @@ static void matrix_multiply(Matrix &out, Matrix const &lhs, Matrix const &rhs) {
     }
     out.reshape(nx, ny);
 
+#pragma omp parallel for collapse(2)
+    for (int y = 0; y < ny; y++) {
+        for (int xBase = 0; xBase < nx; xBase += 32) {
+            // out(x, y) = 0; // 有没有必要手动初始化？ 5 分
+            for (int t = 0; t < nt; t++) {
+                // const float val = rhs(t, y);
+#pragma GCC unroll 4
+                for (int x = xBase; x < xBase + 32; ++x) {
+                    out(x, y) += lhs(x, t) * rhs(t, y);
+                    // _mm_stream_si32((int *)&out(x, y), *(int *)&in(x,y));
+                }
+            }
+        }
+    }
+/*
     // 这个循环为什么不够高效？如何优化？ 15 分
 #pragma omp parallel for collapse(2)
     for (int y = 0; y < ny; y++) {
         for (int x = 0; x < nx; x++) {
-            out(x, y) = 0;  // 有没有必要手动初始化？ 5 分
+            // out(x, y) = 0;  // 有没有必要手动初始化？ 5 分
             for (int t = 0; t < nt; t++) {
                 out(x, y) += lhs(x, t) * rhs(t, y);
             }
         }
     }
+*/
     TOCK(matrix_multiply);
 }
 
@@ -78,7 +132,9 @@ static void matrix_multiply(Matrix &out, Matrix const &lhs, Matrix const &rhs) {
 static void matrix_RtAR(Matrix &RtAR, Matrix const &R, Matrix const &A) {
     TICK(matrix_RtAR);
     // 这两个是临时变量，有什么可以优化的？ 5 分
-    Matrix Rt, RtA;
+    // static thread_local Matrix Rt, RtA;
+    // Matrix Rt, RtA;
+    static Matrix Rt(1120, 1120), RtA(1120, 1120);
     matrix_transpose(Rt, R);
     matrix_multiply(RtA, Rt, A);
     matrix_multiply(RtAR, RtA, R);
@@ -89,7 +145,7 @@ static float matrix_trace(Matrix const &in) {
     TICK(matrix_trace);
     float res = 0;
     size_t nt = std::min(in.shape(0), in.shape(1));
-#pragma omp parallel for reduction(+:res)
+#pragma omp parallel for reduction(+ : res)
     for (int t = 0; t < nt; t++) {
         res += in(t, t);
     }
